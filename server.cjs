7489f58be90b95777832741a346c595f9e0d00db
ezihe-build <olivialane8884@gmail.com>
2026-03-11 06:52:21 +0100

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -8,8 +8,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Handle all routes by serving index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname,'index.html'));
 });
 
 app.listen(PORT, () => {
